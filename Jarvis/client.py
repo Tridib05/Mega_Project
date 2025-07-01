35bf8f9048468c1cacf0ca856d5eca15a81dc23b
Tridib Bhunia <tridibbhunia17@gmail.com>
2025-07-01 21:58:21 +0530

    Update client.py

--- Jarvis/client.py
+++ Jarvis/client.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai 
-genai.configure(api_key="")
-model = genai.GenerativeModel("gemini-2.5-flash")
-response = model.generate_content("You are a virtual assistant named Jarvis.")
-print(response.text)
+genai.configure(api_key="Your_API_Key_Here")  # Replace with your actual API key
+
+model = genai.GenerativeModel("gemini-2.5-flash")       
+response = model.generate_content("You are a virtual assistant named Jarvis.")      
+print(response.text)        
